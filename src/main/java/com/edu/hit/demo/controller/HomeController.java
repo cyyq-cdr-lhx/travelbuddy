66eca1946a60f5439cd2e06a15a58d0068a26e37
heson <1790852382@qq.com>
2024-06-26 13:47:44 +0800

    git switch to dev.

--- src/main/java/com/edu/hit/demo/controller/HomeController.java
+++ src/main/java/com/edu/hit/demo/controller/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-@SessionAttributes("homeUser")
+
 @Controller
 @SessionAttributes("homeUser")
 
