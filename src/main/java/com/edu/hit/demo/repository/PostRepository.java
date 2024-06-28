855a28f5aebb4699b0d1d401a3f9c3d253f2465e
yoki <3467792742@qq.com>
2024-06-28 22:12:15 +0800

    demo1

--- src/main/java/com/edu/hit/demo/repository/PostRepository.java
+++ src/main/java/com/edu/hit/demo/repository/PostRepository.java
@@ -2,13 +2,17 @@ package com.edu.hit.demo.repository;
 
 import com.edu.hit.demo.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByOrderByLikesDesc();
-    List<Post> findByEmail(String email);
+    //List<Post> findAllByOrderByLikesDesc();
+    //List<Post> findByEmail(String email);
+    @Query("SELECT p FROM Post p WHERE p.email = :email ORDER BY p.likes ASC")
+    List<Post> findByEmail(@Param("email") String email);
 
 
 
--- src/main/java/com/edu/hit/demo/service/PostService.java
+++ src/main/java/com/edu/hit/demo/service/PostService.java
@@ -1,10 +1,10 @@
 package com.edu.hit.demo.service;
 
 import com.edu.hit.demo.model.Comment;
-import com.edu.hit.demo.model.Image;
+//import com.edu.hit.demo.model.Image;
 import com.edu.hit.demo.model.Post;
 import com.edu.hit.demo.repository.CommentRepository;
-import com.edu.hit.demo.repository.ImageRepository;
+//import com.edu.hit.demo.repository.ImageRepository;
 import com.edu.hit.demo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,8 +17,6 @@ public class PostService {
     @Autowired
     private PostRepository postRepository;
 
-    @Autowired
-    private ImageRepository imageRepository;
 
     @Autowired
     private CommentRepository commentRepository;
@@ -27,12 +25,7 @@ public class PostService {
         return postRepository.findAll();
     }
 
-    public int likePost(Long id) {
-        Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("Post not found"));
-        post.setLikes(post.getLikes() + 1);
-        postRepository.save(post);
-        return post.getLikes();
-    }
+
 
     public void commentPost(Long id, String text) {
         Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("Post not found"));
@@ -46,21 +39,11 @@ public class PostService {
         postRepository.save(post);
     }
 
-    public List<Post> findAllOrderByLikesDesc() {
-        // 使用方法命名规则
-        return postRepository.findAllByOrderByLikesDesc();
-
-        // 或者使用 @Query 注解方式
-        // return postRepository.findAllOrderByLikesDesc();
-    }
 
     public Post getPostById(Long postId) {
         return postRepository.findById(postId).orElseThrow();
     }
 
-    public List<Image> getImagesByPost(Post post) {
-        return imageRepository.findByPost(post);
-    }
 
     public List<Comment> getCommentsByPost(Post post){
         return commentRepository.findByPost(post);
@@ -74,6 +57,17 @@ public class PostService {
         return commentRepository.findByEmail(email);
     }
 
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
 
+    public void likePost(Post post) {
+        post.setLikes(post.getLikes() + 1);
+        postRepository.save(post);
+    }
 
+    public void notLikePost(Post post) {
+        post.setLikes(post.getLikes() - 1);
+        postRepository.save(post);
+    }
 }
